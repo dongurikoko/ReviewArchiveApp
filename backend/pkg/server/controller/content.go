7ac7fe72f6486748f8e3deaad52088c5723085c9
dongurikoko <108347471+dongurikoko@users.noreply.github.com>
2024-01-04 17:43:30 +0900

    Revert "Revert "content/create実装""

--- backend/pkg/server/controller/content.go
+++ backend/pkg/server/controller/content.go
@@ -1,1 +1,66 @@
 package controller
+
+import (
+	"fmt"
+
+	"reviewArchive/pkg/server/model"
+)
+
+type ContentRequest struct {
+	Title       string
+	Before_code string
+	After_code  string
+	Review      string
+	Memo        string
+	Keywords    []string
+}
+
+type ContentController struct {
+	ContentRepository model.ContentRepositoryInterface
+	KeywordRepository model.KeywordRepositoryInterface
+}
+
+func NewContentContoroller(contentRepository model.ContentRepositoryInterface,
+	keywordRepository model.KeywordRepositoryInterface) *ContentController {
+	return &ContentController{
+		ContentRepository: contentRepository,
+		KeywordRepository: keywordRepository,
+	}
+}
+
+type ContentControllerInterface interface {
+	ContentCreate(record *ContentRequest) error
+}
+
+// コンテンツ作成ロジック
+func (c *ContentController) ContentCreate(record *ContentRequest) error {
+	recordContent := &model.Content{
+		Title:       record.Title,
+		Before_code: record.Before_code,
+		After_code:  record.After_code,
+		Review:      record.Review,
+		Memo:        record.Memo,
+	}
+	// コンテンツテーブルへの挿入
+	id, err := c.ContentRepository.InsertContent(recordContent)
+	if err != nil {
+		return fmt.Errorf("failed to InsertContent in ContentCreate: %w", err)
+	}
+
+	// キーワードテーブルへの挿入
+	if err := c.KeywordRepository.InsertKeyword(id, record.Keywords); err != nil {
+		return fmt.Errorf("failed to InsertKeyword in ContentCreate: %w", err)
+	}
+
+	return nil
+}
+
+/* コンテンツ更新ロジック
+func (c *ContentController) ContentUpdate(record *Content_keywords) error {
+	// コンテンツテーブルの更新
+
+	// キーワード削除
+	// キーワード追加
+}*/
+
+// コンテンツ削除ロジック
