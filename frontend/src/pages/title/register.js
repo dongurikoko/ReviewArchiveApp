444319d89fcb314689e9d019e27ec4a923e09eea
dongurikoko <oomotoshiori@oomotoshiorinoMacBook-Pro.local>
2024-01-14 02:02:55 +0900

    コンテンツの新規作成の実装

--- frontend/src/pages/title/register.js
+++ frontend/src/pages/title/register.js
@@ -1,28 +1,71 @@
+import {useState} from "react";
+
 const Register = () => {
 
-    const handleSubmit = () => {
+    const [newContent, setNewContent] = useState({
+        title: "",
+        before_code: "",
+        after_code: "",
+        review: "",
+        memo: "",
+        keywords: [] //配列として初期化,
+    })
+
+    const handleContentChange = (e) => {
+        if (e.target.name === 'keyword') {
+            // キーワードフィールドの変更時は、キーワードを配列に分割
+            setNewContent({
+                ...newContent,
+                keywords: e.target.value.split(',').map(kw => kw.trim()), // カンマで分割し、トリムする
+            });
+        } else {
+            setNewContent({
+                ...newContent,
+                [e.target.name]: e.target.value,
+            })
+        }
+    }
+
+        
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
         try{
-            fetch("http://localhost:8080/content/create",{
+            const response = await fetch("http://localhost:8080/content/create",{
                 method: "POST",
                 headers: {
                     "Accept": "application/json",
                     "Content-Type": "application/json",
                 },
-                body:"ダミー"
+                body: JSON.stringify(newContent),
             })
-        }catch(err){}
+            
+            const jsonResponse = await response.json()
+            console.log (jsonResponse)
+            alert(jsonResponse.message)
+
+        }catch(err){
+            alert("ユーザー登録失敗")
+        }
     }
     return(
         <div>
             <h1>コンテンツの新規作成</h1>
             <form onSubmit={handleSubmit}>
-                <input type="text" name="title" placeholder="タイトル" required/>
-                <input type="text" name="before_code" placeholder="修正前コード"/>
-                <input type="text" name="after_code" placeholder="修正後コード"/>
-                <input type="text" name="review" placeholder="レビュー"/>
-                <input type="text" name="memo" placeholder="メモ"/>
-                <input type="text" name="keyword" placeholder="キーワード" required/>
-                <button type="submit">登録</button>
+                <input value={newContent.title} onChange={handleContentChange} 
+                type="text" name="title" placeholder="タイトル" required/>
+                <input value={newContent.before_code} onChange={handleContentChange}
+                type="text" name="before_code" placeholder="修正前コード"/>
+                <input value={newContent.after_code} onChange={handleContentChange}
+                type="text" name="after_code" placeholder="修正後コード"/>
+                <input value={newContent.review} onChange={handleContentChange}
+                type="text" name="review" placeholder="レビュー"/>
+                <input value={newContent.memo} onChange={handleContentChange}
+                type="text" name="memo" placeholder="メモ"/>
+                <input value={newContent.keywords.join(', ')} onChange={handleContentChange}
+                type="text" name="keyword" placeholder="キーワード（カンマ区切り）" required/>
+
+                <button>登録</button>
             </form>
         </div>
     )
