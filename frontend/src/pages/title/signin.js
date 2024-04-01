1d493f7218dd5df30d79d61f59369215bc85557c
dongurikoko <oomotoshiori@oomotoshiorinoMacBook-Pro.local>
2024-04-01 22:33:11 +0900

    signinのUI修正

--- frontend/src/pages/title/signin.js
+++ frontend/src/pages/title/signin.js
@@ -3,17 +3,21 @@ import React from 'react';
 
 const SignIn = () => {
     return (
-        <div>
-            <h2>Sign In</h2>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+            <h1 style={{ marginTop: '10px' }}>Sign In</h1>
             <form>
-                <label>
-                    Email:
-                    <input type="email" name="email" />
-                </label>
-                <label>
-                    Password:
-                    <input type="password" name="password" />
-                </label>
+                <div>
+                    <label>
+                        Email:
+                        <input type="email" name="email" />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Password:
+                        <input type="password" name="password" />
+                    </label>
+                </div>
                 <input type="submit" value="Submit" />
             </form>
             <p>Don't have an account? <a href="/signup">Sign Up</a></p>
