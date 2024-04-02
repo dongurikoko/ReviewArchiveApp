43381623ffe0d268650d79a6d9795669895daf30
dongurikoko <oomotoshiori@oomotoshiorinoMacBook-Pro.local>
2024-04-03 02:08:10 +0900

    logout追加

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,7 +1,21 @@
+import React from 'react';
 import { Link } from "react-router-dom"
 import headerImage from '../images/icon2.png';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../main';
 
 const Header = ({ searchTerm, setSearchTerm }) => {
+    const navigate = useNavigate();
+    const handleLogout = async () => {
+        try {
+            await auth.signOut();
+            localStorage.removeItem('jwt');
+            navigate("/signin");
+        } catch (err) {
+            alert(err.message);
+            console.error(err);
+        }
+    };
     return (
         <header className="header">
             <div><Link to="/"><img src={headerImage} alt="header"/></Link></div>
@@ -10,6 +24,7 @@ const Header = ({ searchTerm, setSearchTerm }) => {
                     <li className="navLink"><Link to="/content/new">レビュー新規作成</Link></li>
                     <li><input type="text" value={searchTerm} onChange={event => setSearchTerm(event.target.value)}
                     placeholder="🔍  キーワード検索"/></li>
+                    <button onClick={handleLogout}>Logout</button>
                 </ul>
             </nav>
         </header>
